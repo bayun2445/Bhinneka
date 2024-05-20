88414f23f2b25cd58b88926cc82ae4d27c27137f
bayun2445 <52805985+bayun2445@users.noreply.github.com>
2024-05-20 10:59:05 +0700

    added UI main page

---
+++ app/src/main/java/com/bayu/bhinneka/ui/main/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.bayu.bhinneka.ui.main
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bayu.bhinneka.R
+import com.bayu.bhinneka.databinding.ActivityMainBinding
+import com.bayu.bhinneka.ui.add_jajanan.AddJajananActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        setSupportActionBar(binding.topBarMenu)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        supportActionBar?.title = getString(R.string.app_name)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.menu_edit -> {
+                startActivity(
+                    Intent(this@MainActivity, AddJajananActivity::class.java)
+                )
+
+                true
+            }
+            else -> true
+        }
+    }
+}
